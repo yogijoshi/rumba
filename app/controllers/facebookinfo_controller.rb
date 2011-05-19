fcf746fc1d8abcb396f1473a5d21cf65170683d5
yogijoshi <yogi.joshi@live.com>
2011-05-19 19:24:49 +0530

    user model created

--- app/controllers/facebookinfo_controller.rb
+++ app/controllers/facebookinfo_controller.rb
@@ -1,18 +1,65 @@
+require 'digest/md5'
 class FacebookinfoController < ApplicationController
-  def index2
-       @oa = Koala::Facebook::OAuth.new('201875599851846', 'e3ce956f923f21edb8fcbba30237f41a')
-
-       @o = @oa.get_user_info_from_cookies(cookies)
-       @oauth = @oa.get_user_from_cookies(cookies)
-
-       @graph = Koala::Facebook::GraphAPI.new()
-       @gr=@graph.get_object("koppel")
-  end
   def index
-      graph = Koala::Facebook::GraphAPI.new(@facebook_cookies["access_token"])
+      @graph = Koala::Facebook::GraphAPI.new(@facebook_cookies["access_token"])
       #@likes = graph.get_connections("me", "likes").to_
-      @mehash = graph.get_object("me")
+      #@mehash = @graph.get_object("me")
+      if Dealtadkauser.find_by_uid(@facebook_cookies["uid"])
+         flash[:message] = "user already exists"
+      else
+        user_hash = create_user_hash_fromfb()
+      end
   end
   def login
+  end
+  def create_user_hash_fromfb
+    user_details = @graph.get_object("me")
+    t = Time.now()
+    user_hash = {
+                  :birthday => user_details["birthday"],
+                  :created => "%.0f" % t.to_f,
+                  :modified => "%.0f" % t.to_f,
+                  :status => "1",
+                  :created_detail =>
+                    {
+                      :seconds => t.sec,
+                      :minutes => t.min,
+                      :hours   => t.hour,
+                      :mday    => t.day,
+                      :wday    => t.wday,
+                      :mon     => t.month,
+                      :year    => t.year,
+                      :yday    => t.yday,
+                      :weekday => t.strftime('%A'),
+                      :month   => t.strftime('%b')
+                    },
+                  :email =>
+                    {
+                      :id => user_details["email"],
+                      :code => "0",
+                      :verified => "0"
+                    },
+                  :facebook =>
+                    {
+                      :access_token => @facebook_cookies["access_token"],
+                      :uid => @facebook_cookies["uid"],
+                      :permissions => "user_birthday,email,user_location,read_stream,publish_stream,offline_access,manage_pages",
+                    },
+                  :fbid => @facebook_cookies["uid"],
+                  :first_name => user_details["first_name"],
+                  :gender => user_details["gender"],
+                  :last_name => user_details["last_name"],
+                  :link => user_details["link"],
+                  :locale => user_details["locale"],
+                  :name => user_details["name"],
+                  :timezone => user_details["timezone"],
+                  :type => "USER",
+                  :updated_time => user_details["updated_time"]
+                }
+      if Dealtadkauser.create(user_hash)
+        flash[:message] = "user created successfully"
+      else
+        flash[:message] = "user not created"
+      end  
   end
 end
--- app/models/dealtadkauser.rb
+++ app/models/dealtadkauser.rb
@@ -1,50 +1,60 @@
 class Dealtadkauser
-
-   "birthday": "08\/29\/1989",
-   "created":             1305009504      ,
-   "created_detail": {
-     "seconds": 24,
-     "minutes": 8,
-     "hours": 12,
-     "mday": 10,
-     "wday": 2,
-     "mon": 5,
-     "year": 2011,
-     "yday": 129,
-     "weekday": "Tuesday",
-     "month": "May",
-     "0":             1305009504
-  },
-   "deal_id": "4dc7df1625c3a15653000000",
-   "email": {
-     "id": "pranayagarwal29081989@gmail.com",
-     "code": "ce7c471c1ba05b7bc95a80e6ffa84d93",
-     "verified": 0
-  },
-   "facebook": {
-     "access_token": "186490868052811|2.a1Sy5Qnzaon3rlFYsdhrog__.3600.1305014400.1-535299395|3MvA8iOMTzfFyaehtJ2z6mzrYrE",
-     "uid": "535299395",
-     "permissions": "user_birthday,email"
-  },
-   "fbid": "535299395",
-   "first_name": "Pranay",
-   "gender": "male",
-   "last_name": "Agarwal",
-   "link": "http: \/\/www.facebook.com\/agarwalpranay",
-   "locale": "en_US",
-   "modified":             1305009504      ,
-   "name": "Pranay Agarwal",
-   "ref": "628842285,123245771051583",
-   "referrerScore": 10,
-   "referrerStat": {
-     "latestReferrenceTime": "6 days ago",
-     "directReference": 1,
-     "indirectdirectReference": 0,
-     "referrerScore": 10
-  },
-   "status": 1,
-   "store_id": "4dc1093a25c3a1bb46000000",
-   "timezone": 5.5,
-   "type": 2,
-   "updated_time": "2011-05-10T06: 38: 22+0000"
+    include MongoMapper::Document
+   #"birthday": "08\/29\/1989",
+#   "created":             1305009504      ,
+#   "created_detail": {
+#     "seconds": 24,
+#     "minutes": 8,
+#     "hours": 12,
+#     "mday": 10,
+#     "wday": 2,
+#     "mon": 5,
+#     "year": 2011,
+#     "yday": 129,
+#     "weekday": "Tuesday",
+#     "month": "May",
+#     "0":             1305009504
+#  },
+#   "deal_id": "4dc7df1625c3a15653000000",
+#   "email": {
+#     "id": "pranayagarwal29081989@gmail.com",
+#     "code": "ce7c471c1ba05b7bc95a80e6ffa84d93",
+#     "verified": 0
+#  },
+#   "facebook": {
+#     "access_token": "186490868052811|2.a1Sy5Qnzaon3rlFYsdhrog__.3600.1305014400.1-535299395|3MvA8iOMTzfFyaehtJ2z6mzrYrE",
+#     "uid": "535299395",
+#     "permissions": "user_birthday,email"
+#  },
+#   "fbid": "535299395",
+#   "first_name": "Pranay",
+#   "gender": "male",
+#   "last_name": "Agarwal",
+#   "link": "http: \/\/www.facebook.com\/agarwalpranay",
+#   "locale": "en_US",
+#   "modified":             1305009504      ,
+#   "name": "Pranay Agarwal",
+#   "ref": "628842285,123245771051583",
+#   "referrerScore": 10,
+#   "referrerStat": {
+#     "latestReferrenceTime": "6 days ago",
+#     "directReference": 1,
+#     "indirectdirectReference": 0,
+#     "referrerScore": 10
+#  },
+#   "status": 1,
+#   "store_id": "4dc1093a25c3a1bb46000000",
+#   "timezone": 5.5,
+#   "type": 2,
+#   "updated_time": "2011-05-10T06: 38: 22+0000"
+  def self.create(dealtadkauser_hash)
+    dealtadkauser = Dealtadkauser.new(dealtadkauser_hash)
+    dealtadkauser.save!
+  end
+  def self.find_by_uid(uid)
+    if Dealtadkauser.where(:fbid => uid).to_a != nil
+      return true
+    end
+    return false
+  end
 end
