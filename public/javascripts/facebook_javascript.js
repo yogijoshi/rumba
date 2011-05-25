dc017bf03ad0c94722a805aa4b31ede9b187d784
yogijoshi <yogi.joshi@live.com>
2011-05-25 14:15:36 +0530

    jdfjdfjdhfj# with '#' will be ignored, and an empty message aborts the commit.

--- public/javascripts/facebook_javascript.js
+++ public/javascripts/facebook_javascript.js
@@ -1,15 +1,7 @@
 
-  window.fbAsyncInit = function() {
-    FB.init({appId: '214912208528198', status: true, cookie: true, xfbml: true});
-    FB.Event.subscribe('auth.logout', function(response) {
-      // do something with response
-      check_login_status();
-    });
-
-  };
-
   function check_login_status()
   {
+    alert("Hi");
     FB.getLoginStatus(function(response) {
       if (response.session) {
         if (response.perms) {
@@ -30,101 +22,9 @@
           }else {
             alert("No Login");
           }
-        }, {perms:'user_birthday,email,user_location,read_stream,publish_stream,offline_access,manage_pages'});
+        }, {perms:'read_stream,publish_stream,offline_access,manage_pages'});
       }
     });
   }
 
-  (function() {
-    var e = document.createElement('script');
-    e.type = 'text/javascript';
-    e.src = document.location.protocol +
-      '//connect.facebook.net/en_US/all.js';
-    e.async = true;
-    document.getElementById('fb-root').appendChild(e);
-  }());
-
-  function login(){
-    alert("Hi");
-  }
-  //stream publish method
-  function streamPublish(name, description, hrefTitle, hrefLink, userPrompt){
-    FB.ui(
-    {
-      method: 'stream.publish',
-      message: '',
-      attachment: {
-        name: name,
-        caption: '',
-        description: (description),
-        href: hrefLink
-      },
-      action_links: [
-        { text: hrefTitle, href: hrefLink }
-      ],
-      user_prompt_message: userPrompt
-    },
-    function(response) {
-
-    });
-
-  }
-  function showStream(){
-    FB.api('/me', function(response) {
-      //console.log(response.id);
-      streamPublish(response.name, 'Thinkdiff.net contains geeky stuff', 'hrefTitle', 'http://thinkdiff.net', "Share thinkdiff.net");
-    });
-  }
-
-  function share(){
-    var share = {
-      method: 'stream.share',
-      u: 'http://thinkdiff.net/'
-    };
-
-    FB.ui(share, function(response) { console.log(response); });
-  }
-
-  function graphStreamPublish(){
-    var body = 'Reading New Graph api & Javascript Base FBConnect Tutorial from Thinkdiff.net';
-    FB.api('/me/feed', 'post', { message: body }, function(response) {
-      if (!response || response.error) {
-        alert('Error occured');
-      } else {
-        alert('Post ID: ' + response.id);
-      }
-    });
-  }
-
-  function fqlQuery(){
-    FB.api('/me', function(response) {
-      var query = FB.Data.query('select name, hometown_location, sex, pic_square from user where uid={0}', response.id);
-      query.wait(function(rows) {
-
-        document.getElementById('name').innerHTML =
-          'Your name: ' + rows[0].name + "<br />" +
-          '<img src="' + rows[0].pic_square + '" alt="" />' + "<br />";
-      });
-    });
-  }
-
-  function setStatus(){
-    status1 = document.getElementById('status').value;
-    FB.api(
-    {
-      method: 'status.set',
-      status: status1
-    },
-    function(response) {
-      if (response == 0){
-        alert('Your facebook status not updated. Give Status Update Permission.');
-      }
-      else{
-        alert('Your facebook status updated');
-      }
-    }
-  );
-  }
-
-
-
+  
