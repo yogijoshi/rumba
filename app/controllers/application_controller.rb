7f416c331108cbef29491432f1cc12fdd049cc36
tejas <inboxtejas@gmail.com>
2011-05-25 15:55:21 +0530

    config file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
- 
+
+
   before_filter :parse_facebook_cookies
   def parse_facebook_cookies
     # = Koala::Facebook::OAuth.new('201875599851846', 'e3ce956f923f21edb8fcbba30237f41a')
